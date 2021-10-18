57cdc6fce0f7982b8f1af38a407ab677521b2bd1
zhaohankai <1119605662@qq.com>
2021-10-18 15:28:42 +0800

    第二次提交

---
+++ src/test/java/com/nowcoder/community/MailTest.java
@@ -0,0 +1,40 @@
+package com.nowcoder.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.nowcoder.community.util.MailClient;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTest {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("1119605662@qq.com", "Test", "welcome");
+    }
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("1119605662@qq.com", "HTML", content);
+    }
+}
