74b3cf8dafa6bc86f94f86c8cc21c6c4b26bb766
zhaohankai <1119605662@qq.com>
2021-11-01 11:43:48 +0800

    第四次提交 实现了点赞，关注，粉丝列表，使用redis重构了登录模块，登录凭证等在redis里保存

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -4,12 +4,60 @@ public class RedisKeyUtil {
 
     //拼接字符
     private static final String SPLIT = ":";
-    //前缀
+    // 实体赞前缀
     private static final String PREFIX_ENTITY_LIKE = "like:entity";
+    // 用户前缀
+    private static final String PREFIX_USER_LIKE = "like:user";
+    //被关注的目标
+    private static final String PREFIX_FOLLOWEE = "followee";
+    //关注的粉丝
+    private static final String PREFIX_FOLLOWER = "follower";
+    //验证码
+    private static final String PREFIX_KAPTCHA = "kaptcha";
+    //登录凭证
+    private static final String PREFIX_TICKET = "ticket";
+    //用户
+    private static final String PREFIX_USER = "user";
+
+
+
 
     //某个实体的赞
     //like:entity:entityType:entityId -> set(userId)
     public static String getEntityLikeKey(int entityType, int entityId) {
         return PREFIX_ENTITY_LIKE + SPLIT + entityType + SPLIT + entityId;
+    }
+
+    //某一个用户的赞
+    //like:user:userId -> int
+    public static String getUserLikeKey(int userId) {
+        return PREFIX_USER_LIKE + SPLIT + userId;
+    }
+
+    //某个用户关注的实体
+    //followee:userId:entityType -> zset(entityId, now)
+    public static String getFolloweeKey(int userId, int entityType) {
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    //某个实体拥有的粉丝
+    //follower:entityType:entityId -> zset(userId, now)
+    public static String getFollowerKey(int entityType, int entityId) {
+        return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT + entityId;
+    }
+
+    //登录验证码
+    public static String getKaptchaKey(String owner) {
+        return PREFIX_KAPTCHA + SPLIT + owner;
+    }
+
+    //登录的凭证
+    public static String getTicketKey(String ticket) {
+        return PREFIX_TICKET + SPLIT + ticket;
+    }
+
+    //用户
+    public static String getUserKey(int userId) {
+        return PREFIX_USER + SPLIT + userId;
     }
 }
